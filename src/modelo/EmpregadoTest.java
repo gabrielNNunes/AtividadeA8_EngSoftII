bee427172f6a769486d31920316d15ed7b6f30f9
Thomas Edson Nunes <86779464+ThomasEdsonNunes@users.noreply.github.com>
2021-07-06 08:45:48 -0300

    Criando teste para a regra do SetHoras até 40 horas

--- src/modelo/EmpregadoTest.java
+++ src/modelo/EmpregadoTest.java
@@ -15,7 +15,7 @@ public class EmpregadoTest {
       empregado = new Empregados("Gabriel Nunes");
     }
   
-//=======================================================================================================	
+//=======================================================================================================RealizarPagamento	
   	@Test
 	public void realizarPagamentoEmpregadoMaiorQue1100() throws Exception {
 		ModeloEmpregado empre = new Empregados("Teste");
@@ -23,7 +23,7 @@ public class EmpregadoTest {
 		empre.setHoras(10);
 		Assertions.assertEquals(1500.00,empre.realizarPagamento());
 	}
-//=======================================================================================================	
+//=======================================================================================================RealizarPagamento	
 	@Test
 	public void realizarPagamentoEmpregadoMenorQue1100() throws Exception{
 		ModeloEmpregado empre = new Empregados("Teste");
@@ -32,7 +32,7 @@ public class EmpregadoTest {
 		
 		Assertions.assertEquals(1100.00,empre.realizarPagamento());
 	}
-//=======================================================================================================
+//=======================================================================================================RealizarPagamento
 	@Test
 	public void realizarPagamentoEmpregadoIgualA1100() throws Exception{
 		ModeloEmpregado empre = new Empregados("Teste");
@@ -41,7 +41,7 @@ public class EmpregadoTest {
 		
 		Assertions.assertEquals(1100.00,empre.realizarPagamento());
 	}
-//=======================================================================================================
+//=======================================================================================================RealizarPagamento
 	@Test
 	public void realizarPagamentoEmpregadoValorPorHoraIgualA0(){
 		String mensagemErro = "";
@@ -54,7 +54,7 @@ public class EmpregadoTest {
 		}
 		Assertions.assertEquals("O valor por hora deve ser maior que 0",mensagemErro);
 	}
-//=======================================================================================================
+//=======================================================================================================RealizarPagamento
 	@Test
 	public void realizarPagamentoEmpregadoHoraIgualA0(){
 		String mensagemErro = "";
@@ -67,8 +67,25 @@ public class EmpregadoTest {
 		}
 		Assertions.assertEquals("A quantidade de horas deve ser maior que 0",mensagemErro);
 	}
-  //=======================================================================================================	
-  
+//=======================================================================================================SetHoras	
+  	@Test
+	public void TesteSetHorasEmpregado() throws Exception {
+		empregado.setHoras(30);
+		Assertions.assertEquals(30,empregado.getHoras());
+	}
+//=======================================================================================================SetHoras
+	@Test
+	public void TesteSetHorasEmpregadoMaiorQue40() {
+		String MensagemErro = "";
+		try {
+		empregado.setHoras(45);
+		}catch (Exception e) {
+			MensagemErro =  e.getMessage();
+		}
+		Assertions.assertEquals("O número de horas trabalhadas por funcionários próprios deve ser menor ou igual a 40.",MensagemErro);
+	}
+//=======================================================================================================	
+
   
   
   
