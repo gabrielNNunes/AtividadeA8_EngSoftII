d605806bb4a613a04529b27a3fc9f854489f5c78
Thomas Edson Nunes <86779464+ThomasEdsonNunes@users.noreply.github.com>
2021-07-05 22:59:51 -0300

    Métodos de teste para validar o pagamento dos Empregados Terceirizados
    Foi criado métodos para validar:
        Salário menor que 1100,00;
        Salário igual a 1100,00;
        Salário maior a 1100,00;
        Valor por Hora igual a 0;
        Valor da hora trabalhada igual a 0;
        Valor do adicional igual a 0.

--- src/modelo/EmpregadoTerceirizadoTest.java
+++ src/modelo/EmpregadoTerceirizadoTest.java
@@ -1,1 +1,94 @@
+package modelo;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class EmpregadoTerceirizadoTest {
+
+	
+	EmpregadoTerceirizado empregadoTerceirizado;
+	
+	
+	@BeforeEach
+	private void instanciarObjetos() {		
+		empregadoTerceirizado = new EmpregadoTerceirizado("Gabriel Nunes");
+	}
+  //=======================================================================================================
+	@Test
+	public void realizarPagamentoEmpregadoTerceirizadoMaiorQue1100() throws Exception {
+		EmpregadoTerceirizado empreTer = new EmpregadoTerceirizado("Teste");
+		empreTer.setValorPorHora(150.0);
+		empreTer.setHoras(10);
+		empreTer.setAdicional(200.0);
+		Assertions.assertEquals(1700.00,empreTer.realizarPagamento());
+	}
+//=======================================================================================================	
+	@Test
+	public void realizarPagamentoEmpregadoTerceirizadoMenorQue1100() throws Exception{
+		empregadoTerceirizado.setValorPorHora(30.0);
+		empregadoTerceirizado.setHoras(30);
+		empregadoTerceirizado.setAdicional(100.00);
+		Assertions.assertEquals(1100.00,empregadoTerceirizado.realizarPagamento());
+	}
+//=======================================================================================================
+	@Test
+	public void realizarPagamentoEmpregadoTerceirizadoIgualA1100() throws Exception{
+		empregadoTerceirizado.setValorPorHora(100.0);
+		empregadoTerceirizado.setHoras(10);
+		empregadoTerceirizado.setAdicional(100.00);
+		Assertions.assertEquals(1100.00,empregadoTerceirizado.realizarPagamento());
+	}
+//=======================================================================================================
+
+	@Test
+	public void realizarPagamentoEmpregadoTerceirizadoValorPorHoraIgualA0(){
+		String mensagemErro = "";
+		try {
+		EmpregadoTerceirizado empreTer = new EmpregadoTerceirizado("Teste");
+		empreTer.setHoras(30);
+    empreTer.setAdicional(100.00);  
+		empreTer.realizarPagamento();
+		}catch (Exception e) {
+			mensagemErro = e.getMessage();
+			
+		}
+		Assertions.assertEquals("O valor por hora deve ser maior que 0",mensagemErro);
+	}
+//=======================================================================================================
+	@Test
+	public void realizarPagamentoEmpregadoTerceirizadoHoraIgualA0(){
+		String mensagemErro = "";
+		try {
+		EmpregadoTerceirizado empreTer = new EmpregadoTerceirizado("Teste");
+		empreTer.setValorPorHora(30.0);
+    empreTer.setAdicional(100.00);  
+		empreTer.realizarPagamento();
+		}catch (Exception e) {
+			mensagemErro = e.getMessage();
+		}
+		Assertions.assertEquals("A quantidade de horas deve ser maior que 0",mensagemErro);
+	}
+//=======================================================================================================
+	@Test
+	public void realizarPagamentoEmpregadoTerceirizadoAdicionalIgualA0(){
+		String mensagemErro = "";
+		try {
+		EmpregadoTerceirizado empreTer = new EmpregadoTerceirizado("Teste");
+		empreTer.setValorPorHora(30.0);
+		empreTer.setHoras(30);
+		empreTer.realizarPagamento();
+		}catch (Exception e) {
+			mensagemErro = e.getMessage();
+		}
+		Assertions.assertEquals("O adicional deve ser maior que 0",mensagemErro);
+		}
+//=======================================================================================================		
+
+  
+  
+  
+  
+  
+  
+}
