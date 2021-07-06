bfb8de5bfd873fdfa44ae4ed58f836a0d56bfc28
Thomas Edson Nunes <86779464+ThomasEdsonNunes@users.noreply.github.com>
2021-07-05 22:16:37 -0300

    Criando métodos de teste para validar o pagamento dos Empregados
    Foi criado métodos para validar:
        Salário menor que 1100,00;
        Salário igual a 1100,00;
        Salário maior a 1100,00;
        Valor por Hora igual a 0;
        Valor da hora trabalhada igual a 0.

--- src/modelo/EmpregadoTest.java
+++ src/modelo/EmpregadoTest.java
@@ -1,1 +1,80 @@
+package modelo;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+
+
+public class TesteEmpregado {
+    ModeloEmpregado empregado;	
+	
+    @BeforeEach
+    private void instanciarObjetos() {
+      empregado = new Empregados("Gabriel Nunes");
+    }
+  
+//=======================================================================================================	
+  	@Test
+	public void realizarPagamentoEmpregadoMaiorQue1100() throws Exception {
+		ModeloEmpregado empre = new Empregados("Teste");
+		empre.setValorPorHora(150.0);
+		empre.setHoras(10);
+		Assertions.assertEquals(1500.00,empre.realizarPagamento());
+	}
+//=======================================================================================================	
+	@Test
+	public void realizarPagamentoEmpregadoMenorQue1100() throws Exception{
+		ModeloEmpregado empre = new Empregados("Teste");
+		empre.setValorPorHora(30.0);
+		empre.setHoras(10);
+		
+		Assertions.assertEquals(1100.00,empre.realizarPagamento());
+	}
+//=======================================================================================================
+	@Test
+	public void realizarPagamentoEmpregadoIgualA1100() throws Exception{
+		ModeloEmpregado empre = new Empregados("Teste");
+		empre.setValorPorHora(100.0);
+		empre.setHoras(11);
+		
+		Assertions.assertEquals(1100.00,empre.realizarPagamento());
+	}
+//=======================================================================================================
+	@Test
+	public void realizarPagamentoEmpregadoValorPorHoraIgualA0(){
+		String mensagemErro = "";
+		try {
+		ModeloEmpregado empre = new Empregados("Teste");
+		empre.setHoras(30);
+		empre.realizarPagamento();
+		}catch (Exception e) {
+			mensagemErro = e.getMessage();
+		}
+		Assertions.assertEquals("O valor por hora deve ser maior que 0",mensagemErro);
+	}
+//=======================================================================================================
+	@Test
+	public void realizarPagamentoEmpregadoHoraIgualA0(){
+		String mensagemErro = "";
+		try {
+		ModeloEmpregado empre = new Empregados("Teste");
+		empre.setValorPorHora(30.0);
+		empre.realizarPagamento();
+		}catch (Exception e) {
+			mensagemErro = e.getMessage();
+		}
+		Assertions.assertEquals("A quantidade de horas deve ser maior que 0",mensagemErro);
+	}
+  //=======================================================================================================	
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
