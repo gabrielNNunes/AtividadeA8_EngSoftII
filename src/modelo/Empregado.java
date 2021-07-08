76e2145fd3deeceadbb381f6f235c00930a0e85d
Thomas Edson Nunes <86779464+ThomasEdsonNunes@users.noreply.github.com>
2021-07-07 21:24:36 -0300

    Mudando a classe Empregado
    agora estendendo a classe ModeloEmpregado

--- src/modelo/Empregado.java
+++ src/modelo/Empregado.java
@@ -1,45 +1,12 @@
 package modelo;
 
-public class Empregado{
-  String nome;
-	int horas;
-	Double valorPorHora;
+public class Empregado extends ModeloEmpregado{
 
 	public Empregado(String nome) {
-		super();
-		this.nome = nome;
-		this.horas = 0;
-		this.valorPorHora = 0.0;
+		super(nome);
 		
 	}
 	
-  public int getHoras() {
-		return horas;
-	}
-
-	public void setHoras(int horas) throws Exception {
-		if (horas > 40){
-			throw new Exception("O número de horas trabalhadas por funcionários próprios deve ser menor ou igual a 40.");
-		}
-			this.horas = horas;
-	}
-	
-
-	public Double getValorPorHora() {
-		return valorPorHora;
-	}
-
-	public void setValorPorHora(Double valorPorHora) throws Exception {
-		if(valorPorHora<30.0 || valorPorHora > 200.0){
-			throw new Exception("O Valor por hora deve ser de 30 a 200");			
-		}
-		this.valorPorHora = valorPorHora;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-  
 	public double realizarPagamento() throws Exception{
 		if(this.valorPorHora == 0) {
 			throw new Exception("O valor por hora deve ser maior que 0");
