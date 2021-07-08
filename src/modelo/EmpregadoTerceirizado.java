bfcd4fff8452361a0c03dc4a5ce0f5cd31637230
Thomas Edson Nunes <86779464+ThomasEdsonNunes@users.noreply.github.com>
2021-07-07 21:30:26 -0300

    Mudando a classe EmpregadoTerceirizado

--- src/modelo/EmpregadoTerceirizado.java
+++ src/modelo/EmpregadoTerceirizado.java
@@ -1,47 +1,16 @@
 package modelo;
 
-public class EmpregadoTerceirizado{
-	  
-  String nome;
-	int horas;
-	Double valorPorHora;
-	Double adicional;
+public class EmpregadoTerceirizado extends ModeloEmpregado{
+	
+	
 
 	public EmpregadoTerceirizado(String nome) {
-		super();
-		this.nome = nome;
-		this.horas = 0;
-		this.valorPorHora = 0.0;
+		super(nome);
 		this.adicional = 0.0;
 	}
 
-public int getHoras() {
-		return horas;
-	}
 
-	public void setHoras(int horas) throws Exception {
-		if (horas > 40){
-			throw new Exception("O número de horas trabalhadas por funcionários próprios deve ser menor ou igual a 40.");
-		}
-			this.horas = horas;
-	}
-	
-
-	public Double getValorPorHora() {
-		return valorPorHora;
-	}
-
-	public void setValorPorHora(Double valorPorHora) throws Exception {
-		if(valorPorHora<30.0 || valorPorHora > 200.0){
-			throw new Exception("O Valor por hora deve ser de 30 a 200");			
-		}
-		this.valorPorHora = valorPorHora;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-	
+	Double adicional;
 	
 
 	public double realizarPagamento() throws Exception{
